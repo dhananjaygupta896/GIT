e25465f7eb62bf351e1ba631a35d100bd47e65bc
dhananjaygupta896 <dhananjaygupta200@gmail.com>
2023-03-02 17:29:44 +0530

    first version

---
+++ binary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void binary(int a)
+{
+    int b[32], i = 0;
+    while (a)
+    {
+        b[i] = a % 2;
+        a = a / 2;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--)
+    {
+        printf("%d", b[j]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int a;
+    printf("Please enter the no to find binary\n");
+    scanf("%d", &a);
+    binary(a);
+}
